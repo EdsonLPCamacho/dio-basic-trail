5d5ba79463c9bf4a150a26072ba1275026e86f15
Edson Camacho <edsonlpcamacho@gmail.com>
2024-07-23 16:03:07 -0300

    Project Created

---
+++ src/com/project/Main.java
@@ -0,0 +1,35 @@
+package com.project;
+
+
+
+	import java.util.Scanner;
+
+	public class Main {
+
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        // Solicitando e lendo os dados do usuário
+	        System.out.println("Por favor, digite o número da Agência:");
+	        String agencia = scanner.nextLine();
+	        
+	        System.out.println("Por favor, digite o número da Conta:");
+	        int numero = scanner.nextInt();
+	        scanner.nextLine(); // Consumir a nova linha deixada pelo nextInt
+	        
+	        System.out.println("Por favor, digite o nome do Cliente:");
+	        String nomeCliente = scanner.nextLine();
+	        
+	        System.out.println("Por favor, digite o saldo da Conta:");
+	        double saldo = scanner.nextDouble();
+	        
+	        // Exibindo a mensagem final
+	        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, "
+	                        + "sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo 
+	                        + " já está disponível para saque.";
+	        
+	        System.out.println(mensagem);
+
+	        scanner.close();
+	    }
+	}
